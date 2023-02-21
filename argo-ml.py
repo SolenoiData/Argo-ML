1834d3a573e025ac46bf8ffd693d14edacc4c12c
alextintin007 <alextintin007@outlook.com>
2023-02-21 00:01:55 -0500

    edit

--- argo-ml.py
+++ argo-ml.py
@@ -1,76 +1,76 @@
-# librería para separar la información 
+# librería para separar la información
 import splitfolders
-# Librerías estándar 
+# Librerías estándar
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from numpy import random
-import math 
+import math
 
-from tensorflow.keras.layers import Conv2D,Flatten,Dense,MaxPool2D,BatchNormalization,GlobalAveragePooling2D
-from tensorflow.keras.applications.resnet50 import preprocess_input,decode_predictions
-from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, MaxPool2D, BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import Model
 
-# Separar los datos 
-input_folder ="/mnt/vol/flowers" 
+# Separar los datos
+input_folder = "/mnt/vol/flowers"
 output = "/mnt/vol/processed_data"
-splitfolders.ratio(input_folder,output, seed=42, ratio=(.6,.2,.2))
+splitfolders.ratio(input_folder, output, seed=42, ratio=(.6, .2, .2))
 
-img_height,img_width = (224,224)
+img_height, img_width = (224, 224)
 batch_size = 32
 train_data_dir = r"/mnt/vol/processed_data/train"
 valid_data_dir = r"/mnt/vol/processed_data/val"
 test_data_dir = r"/mnt/vol/processed_data/test"
 
-train_datagen = ImageDataGenerator(preprocessing_function = preprocess_input,
-                                  shear_range = 0.2,
-                                  zoom_range = 0.2,
-                                  horizontal_flip=True,
-                                  validation_split=0.4)
-    
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   validation_split=0.4)
+
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size = (img_height, img_width),
-    batch_size = batch_size, 
-    class_mode = 'categorical',
-    subset ='training') #set as training data
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='training')  # set as training data
 
 valid_generator = train_datagen.flow_from_directory(
-    valid_data_dir, #same directory training data
-    target_size = (img_height, img_width),
-    batch_size = batch_size,
-    class_mode = 'categorical',
-    subset = 'validation') #set as validation data 
+    valid_data_dir,  # same directory training data
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='validation')  # set as validation data
 
 test_generator = train_datagen.flow_from_directory(
     test_data_dir,
-    target_size = (img_height, img_width),
-    batch_size = 1,
-    class_mode = 'categorical',
-    subset = 'validation') #set as validation data 
+    target_size=(img_height, img_width),
+    batch_size=1,
+    class_mode='categorical',
+    subset='validation')  # set as validation data
 
 base_model = ResNet50(include_top=False, weights='imagenet')
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
 
-predictions = Dense(train_generator.num_classes,activation='softmax')(x)
-model = Model(inputs=base_model.input,outputs=predictions)
+predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
 for layer in base_model.layers:
-    layer.trainable = False 
+    layer.trainable = False
 
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-model.fit(train_generator,
-                       epochs = 10 )
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_generator, epochs=10)
 
 model.save('/mnt/vol/output/ResNet50_Flowers.h5')
 
-test_loss,test_acc = model.evaluate(test_generator, verbose=2)
+test_loss, test_acc = model.evaluate(test_generator, verbose=2)
 print('\nTest accuracy: ', test_acc)
 
 # Save text in an exterior file
